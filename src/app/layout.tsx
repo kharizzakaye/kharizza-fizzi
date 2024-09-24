5a1e632b2cfe3bfffc221bc3ad60058515e03cd8
Khariza <kharizzakaye@gmail.com>
2024-09-24 17:44:12 +0800

    style elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
 import "./app.css";
+import Header from '@/components/Header';
 
 const alpino = localFont({
   src: '../../public/fonts/Alpino-Variable.woff2',
@@ -19,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable}>
-      <body>{children}</body>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header/>
+
+        <main>
+          {children}
+        </main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
